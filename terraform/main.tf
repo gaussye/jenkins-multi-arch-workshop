9ff48ab7b04a27b3a3cd0337e814bc539e4a736a
Ye <ypy@b0be837455eb.ant.amazon.com>
2023-12-13 21:59:55 +0800

    update with IAC code

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ data "aws_ecrpublic_authorization_token" "token" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  name   = "ex-${basename(path.cwd)}"
+  name   = "jenkins-workshop-eks"
   region = "us-west-2"
 
   vpc_cidr = "10.0.0.0/16"
@@ -50,6 +50,7 @@ locals {
   tags = {
     Blueprint  = local.name
     GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+    karpenter.sh/discovery = local.name
   }
 }
 
