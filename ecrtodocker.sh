725e82dbf00c764faf0f28c7fbfde486b58db3f2
Ye <ypy@b0be837455eb.ant.amazon.com>
2023-12-14 09:16:34 +0800

    test

--- ecrtodocker.sh
+++ ecrtodocker.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+cat /root/.ecr/cache.json
+repos=$(jq .Registries /root/.ecr/cache.json | jq -r 'keys[]')
+
+mkdir /root/.docker
+cat <<EOF > /root/.docker/config.json
+{
+    "auths": {
+EOF
+
+for repoid in $repos
+do
+AuthorizationToken=$(jq --arg repoid $repoid '.Registries[$repoid].AuthorizationToken' /root/.ecr/cache.json)
+ProxyEndpoint=$(jq --arg repoid $repoid '.Registries[$repoid].ProxyEndpoint' /root/.ecr/cache.json)
+echo "      $ProxyEndpoint: {"
+echo "        \"auth\": $AuthorizationToken"
+echo "       },"
+done | tee -a /root/.docker/config.json
+
+#sed -i '$ s/,$//' /root/.docker/config.json
+
+cat <<EOF >> /root/.docker/config.json
+}
+}
+EOF
+
+
+cat /root/.docker/config.json
